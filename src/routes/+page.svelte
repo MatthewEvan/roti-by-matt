36c489a9aa6f1f767d092d21c0cdda16e8ad777e
Matthew E <122356019+MatthewEvan@users.noreply.github.com>
2023-08-11 14:16:58 +0000

    Adding & Testing Svelte Awesome Icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+    import Icon from 'svelte-awesome';
+    // Icons Needed
+    import signIn from 'svelte-awesome/icons/signIn';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Icon data="{signIn}" scale={1.2}/>
