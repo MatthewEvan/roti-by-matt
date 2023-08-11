6bd6912cb22663097c512dd907345af0402d7d0a
Matthew E <122356019+MatthewEvan@users.noreply.github.com>
2023-08-11 14:52:59 +0000

    Adding Assets to Main Page & Testing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin >
+	<link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@10..48,400;10..48,700&family=Dosis:wght@400;500;700&family=Inter:wght@400;700&family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <title>Roti by Matt</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</svelte:head>
+
+<slot />
+
+<style>
+    :global(*, *::before, *::after) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-size: 14px;
+        font-family: "Dosis", sans-serif;
+    }
+    :global(html, body) {
+        width: 100%;
+        height: 100%;
+        background-color: #2F241D;
+        color: #FEFEFE;
+    }
+    :global(a) {
+        text-decoration: none;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
-    import Icon from 'svelte-awesome';
+    import Icon from "svelte-awesome";
     // Icons Needed
+    import externalLink from 'svelte-awesome/icons/externalLink';
     import signIn from 'svelte-awesome/icons/signIn';
+    import shoppingCart from 'svelte-awesome/icons/shoppingCart';
+    import userPlus from 'svelte-awesome/icons/userPlus';
+    import instagram from 'svelte-awesome/icons/instagram'
+    import facebookSquare from 'svelte-awesome/icons/facebookSquare';
+    import githubSquare from 'svelte-awesome/icons/githubSquare'
+
+    // Importing Navbar
+    import Navbar from "$components/Navbar.svelte"
+
+    //Importing Images
+    import BGHeader from "$assets/Background-Header.png";
+    import Image1 from "$assets/Image-1.jpg"
+    import Image2 from "$assets/Image-2.jpg"
+    import Image3 from "$assets/Image-3.jpg"
+    import Image4 from "$assets/Image-4.jpg"
 </script>
 
 <h1>Welcome to SvelteKit</h1>
+<h1>Welcome to Roti</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Icon data="{signIn}" scale={1.2}/>
+<Navbar/>
+
